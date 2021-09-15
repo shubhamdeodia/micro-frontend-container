e6d453bfcfccecce11c9ff86be8627b980190ee2
shubhamdeodia <shub.deodia@gmail.com>
2021-09-15 12:19:21 +0530

    handling routing

--- src/components/MarketingApp.js
+++ src/components/MarketingApp.js
@@ -1,12 +1,28 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+
+    const onNavigate = ({ pathname: nextPathName }) => {
+      const {pathname} = history.location;
+      
+      if(pathname !== nextPathName){
+        history.push(nextPathName)
+      }
+    }
+
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate
+    });
+
+    history.listen(onParentNavigate)
+
+  }, [history, mount]);
 
   return <div ref={ref} />;
 };
