dadb2e861eda02d7a94aa102e7cd60c0eab4009e
shubhamdeodia <shubh.deodia@gmail.com>
2022-04-14 12:03:59 +0530

    ci: updated build dir

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,18 +1,22 @@
-const { merge } = require("webpack-merge");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const commonConfig = require("./webpack.common");
-const packageJson = require("../package.json");
+const { merge } = require('webpack-merge');
+const path = require('path');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const { MARKETING_URL } = process.env;
 
+const BUILD_DIR = path.resolve(__dirname, '../build');
+
 const prodConfig = {
-  mode: "production",
+  mode: 'production',
   output: {
-    filename: "[name].[contenthash].js",
+    path: BUILD_DIR,
+    filename: '[name].[contenthash].js',
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "container",
+      name: 'container',
       remotes: {
         marketing: `marketing@${MARKETING_URL}/remoteEntry.js`,
       },
